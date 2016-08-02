e1919de7daa0c08111a6271534bb1ade79c3f2f7
rob perez <ptelerez@cox.net>
2016-08-02 15:04:25 -0400

    improved User class by creating a separate query method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -5,10 +5,7 @@ class User
 
 	public static function find_all_users()
 	{
-		global $database;
-
-		$result_set = $database->query("SELECT * FROM users");
-		return $result_set;
+		return self::find_this_query("SELECT * FROM users");
 
 	}
 
@@ -16,9 +13,17 @@ class User
 	{
 		global $database;
 
-		$result = $database->query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
-		$found_user = mysqli_fetch_array($result);
+		$result_set = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
+		$found_user = mysqli_fetch_array($result_set);
 		return $found_user;
+	}
+
+	public static function find_this_query($sql)
+	{
+		global $database;
+
+		$result_set = $database->query($sql);
+		return $result_set;
 	}
 
 
